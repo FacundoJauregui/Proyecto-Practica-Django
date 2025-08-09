cc6370ff4d4229474df07ae754e9798e1c418f04
Facundo <facu.jaurey@gmail.com>
2025-08-09 09:35:03 -0300

    Conexion y Creacion de modelos en PostgresSQL

--- App/core/erp/models.py
+++ App/core/erp/models.py
@@ -1,2 +1,52 @@
 from django.db import models
+from datetime import datetime 
 
+class Type(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Tipo",unique=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Tipo"
+        verbose_name_plural = "Tipos"
+        db_table = 'erp_type'
+        ordering = ['name']
+        
+class Category(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name="ID")
+    name = models.CharField(max_length=150, verbose_name="Categoría")
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        verbose_name = "Categoría"
+        verbose_name_plural = "Categorías"
+        db_table = 'erp_category'
+        ordering = ['name']
+        
+class Employee(models.Model):
+    type = models.ForeignKey(Type, on_delete=models.CASCADE, verbose_name="Tipo")
+    id = models.AutoField(primary_key=True, verbose_name="ID") 
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    dni = models.IntegerField(unique=True)
+    email = models.EmailField(unique=True)
+    date_joined = models.DateField(default=datetime.now,verbose_name="Fecha de Registro")
+    age = models.IntegerField(default=18,verbose_name="Edad")
+    phone = models.CharField(max_length=15,verbose_name="Teléfono")
+    address = models.CharField(max_length=100,verbose_name="Dirección")
+    position = models.CharField(max_length=50,verbose_name="Cargo")
+    salary = models.DecimalField(max_digits=10, decimal_places=2, verbose_name="Salario")
+    avatar = models.ImageField(upload_to='avatars/',null= True, blank=True, verbose_name="Avatar")
+    cvitae = models.FileField(upload_to='cvitas/',null= True, blank=True, verbose_name="Curriculum Vitae")
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+    
+    class Meta:
+        verbose_name = "Empleado"
+        verbose_name_plural = "Empleados" 
+        db_table = 'erp_employee'
+        ordering = ['id']
--- App/core/erp/tests.py
+++ App/core/erp/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+# Ahora puedes hacer:
+from Config import wsgi
+# ...resto del código...
+from core.erp.models import Type
 
-# Create your tests here.
+'''# SELECT * FROM erp_type
+
+query = Type.objects.all()
+
+#INSERT INTO erp_type (name) VALUES ('Test Type')
+
+t = Type()
+t.name = "Test Type2"
+t.save()  # Guardar el objeto en la base de datos
+
+#UPDATE erp_type SET name = 'Updated Type' WHERE id = 1
+try :
+    t = Type.objects.get(id=10)
+    t.name = "Updated Type"
+    t.save()  # Guardar los cambios en la base de datos
+except Exception as e:
+    print(f"Error: {e}")
+    
+  
+#DELETE FROM erp_type WHERE id = 1
+try :
+    t = Type.objects.get(id=10)
+    t.delete()  # Eliminar el objeto de la base de datos
+except Exception as e:
+    print(f"Error: {e}")'''
+    
+obj1 = Type.objects.filter(name="Test Type2")
+print(obj1)
+obj2 = Type.objects.filter(name__contains="Test")
+print(obj2)
+obj2 = Type.objects.filter(name__icontains="test")
+print(obj2)
